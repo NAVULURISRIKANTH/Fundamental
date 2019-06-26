2c1ee6bd2c605d33ab1fbe66bdfa2d0dce7d7541
lenovo <lenovo@DESKTOP-F2ECO83>
2019-06-26 10:53:56 +0530

    first

---
+++ Fundamentals/src/exp14.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class exp14 {
+
+		public static void main(String[] args)
+		{	
+			for(int i=23;i<=57;i++)
+			{
+				if(i%2==0)
+				{
+					System.out.println(i);
+				}
+			}
+		}
+	}
