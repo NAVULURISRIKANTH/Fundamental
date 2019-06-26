e4b64fc68ec80099888f4ecae0adcf966eb9cecd
lenovo <lenovo@DESKTOP-F2ECO83>
2019-06-26 11:44:15 +0530

    second

---
+++ Fundamentals/src/exp16.java
@@ -0,0 +1,18 @@
+
+public class exp16 {
+	public static void main(String[] args){
+		int i,n;
+		int c=0;
+		for(n=10;n<=99;n++){
+		for(i=1;i<=n;i++) {
+			if(n%i==0){
+				c++;
+			}
+		}
+		if(c==2){
+		System.out.println(n);
+		c=0;
+	    }
+		}
+	   }
+	}
